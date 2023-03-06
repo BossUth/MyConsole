460c1c752f9ea29c9facd8025f52df6ac94ba66f
BossUth <uthmanuthmannureni@gmail.com>
2023-03-06 13:06:04 +0000

    added it to the program file

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -6,7 +6,7 @@ namespace ConsoleAppProject.App02
     /// This app is for Calculating BMI
     /// </summary>
     /// <author>
-    /// Student Name version 0.1
+    /// Uthman Uthman-Nureni version 0.1
     /// </author>
     /// 
     /// <summary>
@@ -19,14 +19,12 @@ namespace ConsoleAppProject.App02
     /// </summary>
     public class Program
     {
-        public static void Main()
+        public void Main()
         {
             Console.WriteLine(" BMI Calculator - Uthman Uthman.");
 
-            Console.Write("Say your Full Name");
-            string name = Console.ReadLine();
 
-            Console.Write($"Hi {name}, now let's calculate your BMI.");
+            Console.Write($"Hi, now let's calculate your BMI.");
             Console.Write("Enter your weight (KG's): ");
             double weight = Convert.ToDouble(Console.ReadLine());
 
@@ -37,19 +35,29 @@ namespace ConsoleAppProject.App02
             Console.WriteLine($"Your BMI is {BMI:F2}");
 
             if (BMI < 18.5)
-                Console.WriteLine($"Your BMI of {BMI:F2} is deemed as underweight, {name}.");
+                Console.WriteLine($"Your BMI of {BMI:F2} is deemed as underweight.");
             else if (BMI >= 18.5 && BMI <= 24.9)
-                Console.WriteLine($"Your BMI of {BMI:F2} is a healthy weight and normal - Keep at your lifestyle, {name}.");
+                Console.WriteLine($"Your BMI of {BMI:F2} is a healthy weight and normal - Keep at your lifestyle.");
             else if (BMI >= 25 && BMI <= 29.9)
-                Console.WriteLine($"Your BMI of {BMI:F2} is classified as overwiehgt, {name}.");
+                Console.WriteLine($"Your BMI of {BMI:F2} is classified as overwiehgt");
             else
-                Console.WriteLine($"Your BMI f {BMI:F2} is considered obese, {name}.");
+                Console.WriteLine($"Your BMI f {BMI:F2} is considered obese.");
 
 
             Console.Write("You people should focous more on exercising than eatin then you would not be this weight.");
+
+
+            /// Less than 16.0 - Serverley Underweight
+            /// From 16.0 to 18.5 - Under Weight
+            /// From 18.6 to 35.0 - Normal Weight
+            /// From 25.1 to 30.0 - Over Weight
+            /// From 30.1 to 35.0 - Moderately Obese
+            /// More than 35.0 - Severly Obese
         }
 
-      
+
+        
+        
         
 
     }
--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App02;
 using ConsoleAppProject.App03;
 using ConsoleAppProject.Helpers;
 using System;
@@ -25,6 +26,38 @@ namespace ConsoleAppProject
 
             DistanceConverter converter = new DistanceConverter();
             converter.ConvertDistance();
+
+
+            Console.WriteLine("This is the BMI Calculator");
+            Console.WriteLine();
+            Console.Beep();
+
+            Console.WriteLine(" BMI Calculator - Uthman Uthman.");
+
+
+            Console.Write($"Hi, now let's calculate your BMI.");
+            Console.Write("Enter your weight (KG's): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your height (M's): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double BMI = weight / (height * height);
+            Console.WriteLine($"Your BMI is {BMI:F2}");
+
+            if (BMI < 18.5)
+                Console.WriteLine($"Your BMI of {BMI:F2} is deemed as underweight.");
+            else if (BMI >= 18.5 && BMI <= 24.9)
+                Console.WriteLine($"Your BMI of {BMI:F2} is a healthy weight and normal - Keep at your lifestyle.");
+            else if (BMI >= 25 && BMI <= 29.9)
+                Console.WriteLine($"Your BMI of {BMI:F2} is classified as overwiehgt");
+            else
+                Console.WriteLine($"Your BMI f {BMI:F2} is considered obese.");
+
+
+            Console.Write("You people should focous more on exercising than eatin then you would not be this weight.");
         }
+
+        
     }
 }
