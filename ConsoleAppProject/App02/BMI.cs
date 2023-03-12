8da90400bc4e381fbb322882a6218a1970c24b45
BossUth <uthmanuthmannureni@gmail.com>
2023-03-12 20:14:35 +0000

    i have added inches and pounds

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -34,6 +34,31 @@ namespace ConsoleAppProject.App02
             double BMI = weight / (height * height);
             Console.WriteLine($"Your BMI is {BMI:F2}");
 
+
+            Console.WriteLine("Hello now we will callculater your bmi using pounds and inches.");
+            Console.WriteLine("Enter your weight (LBS): ");
+            double weight1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter your height (INCH's): ");
+            double height1 = Convert.ToDouble(Console.ReadLine());
+
+            double BMI1 = weight * 703 / (height1 * height);
+            Console.WriteLine($"Your BMI is {BMI1:F1}");
+
+            if (BMI1 < 18.5)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is deemed as underweight.");
+            else if (BMI1 >= 18.5 && BMI <= 24.9)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is a healthy weight and normal - Keep at your lifestyle.");
+            else if (BMI1 >= 25 && BMI <= 29.9)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is classified as overwiehgt");
+            else if (BMI1 >= 30.0 && BMI <= 34.9)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is considered obese|.");
+            else if (BMI1 >= 35.0 && BMI <= 39.9)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is considered obese||.");
+            else if (BMI1 >= 40.0)
+                Console.WriteLine($"Your BMI of {BMI1:F1} is considered obese|||.");
+
+
             if (BMI < 18.5)
                 Console.WriteLine($"Your BMI of {BMI:F2} is deemed as underweight.");
             else if (BMI >= 18.5 && BMI <= 24.9)
