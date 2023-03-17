77d057585858736f08cc475d4718c0e0543ab083
BossUth <uthmanuthmannureni@gmail.com>
2023-03-17 10:42:00 +0000

    made changes to calculategradeprofile

--- ConsoleAppProject/App03/StudentGrades.cs
+++ ConsoleAppProject/App03/StudentGrades.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LanguageExt.TypeClasses;
+using System;
 using System.Linq;
 using System.Text;
 
@@ -10,6 +11,15 @@ namespace ConsoleAppProject.App03
     /// </summary>
     public class StudentGrades
     {
+
+
+        public const int FIRSTCLASS = 70;
+        public const int UPPERSECONDCLASS = 60;
+        public const int LOWERSECONDCLASS = 50;
+        public const int THIRDCLASS = 40;
+        public const int FAIL = 0;
+
+
         // The Grade Boundaries
 
         public const int LowestMarkLowest = 0;
@@ -22,10 +32,12 @@ namespace ConsoleAppProject.App03
         public int[] Marks { get; set; }
         public int[] GradeProfile { get; set; }
 
-        public int[] Counts = new int[5];
+        public int[] Counts = new int[10];
+
+        public int students;
 
         public int Total { get; set; }
-        public double Mean { get; set; }
+        public int Mean { get; set; }
         public int Minimum { get; set; }
         public int Maximum { get; set; }
 
@@ -33,7 +45,9 @@ namespace ConsoleAppProject.App03
         {
             Students = new string[]
             {
-                "Uthman", "Aadhi", "Crown" ,"entwan"
+                "Uthman", "Aadhi", "Crown" ,"Entwan",
+                "Chole", "Ben", "John", "Sophie",
+                "Johnathon", "Benjamain"
             };
 
             GradeProfile = new int[(int)Grades.A + 1];
@@ -42,10 +56,10 @@ namespace ConsoleAppProject.App03
 
         public void Run()
         {
-            DisplayMenue();
+            DisplayMenu();
         }
 
-        public void DisplayMenue()
+        public void DisplayMenu()
         {
             Console.WriteLine("1. Input Marks");
             Console.WriteLine("2. Output Grades");
@@ -61,24 +75,24 @@ namespace ConsoleAppProject.App03
 
                 if (choice == "1")
                 {
-                    Console.WriteLine("Please Select from the Menue");
+                    Console.WriteLine("Please Select from the Menu");
                     InputMarks();
                     
                 }
                 else if (choice == "2")
                 {
-                    Console.WriteLine("Please Select from the Menue");
+                    Console.WriteLine("Please Select from the Menu");
                     OutputMarks();
                 }
                 else if (choice == "3")
                 {
-                    Console.WriteLine("Please Select from the Menue");
+                    Console.WriteLine("Please Select from the Menu");
                     OutputStatistics();
                 }
                 else if (choice == "4")
                 {
-                    Console.WriteLine("Please Select from the Menue");
-                    //OutputGradeProfile();
+                    Console.WriteLine("Please Select from the Menu");
+                    OutputGradeProfile();
                 }
                 else if (choice == "5")
                 {
@@ -93,10 +107,10 @@ namespace ConsoleAppProject.App03
             Console.WriteLine($"Mean Mark: {Mean}\nMinimum Mark: {Minimum}\nMaximum Mark: {Maximum}");
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Press any key to continue");
+            Console.WriteLine("Press a key");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.ReadKey();
-            DisplayMenue();
+            DisplayMenu();
         }
 
         private void OutputGradeProfile()
@@ -104,19 +118,20 @@ namespace ConsoleAppProject.App03
             CalculateGradeProfile();
             // ADDING UP ALL THE GRADE COUNTS SO I CAN USE IT AS A TOTAL STUDENTS
             int total = Counts.Sum();
+            double total1 = Counts.Sum();
             // PRINTING ON THE SCREEN 
-            Console.WriteLine($" A (First Class) > There are {Counts[0]} students in this range, which is {(double)Counts[0] / total:P}");
-            Console.WriteLine($" B (Upper Second Class) > {Counts[1]} students in this range, which is {(double)Counts[1] / total:P}");
-            Console.WriteLine($" C (Lower Second Class) > {Counts[2]} students in this range, which is  {(double)Counts[1] / total:P}");
-            Console.WriteLine($" D (Third Class) > {Counts[3]} students in this range, which is  {(double)Counts[3] / total:P}");
-            Console.WriteLine($" F (Fail) > {Counts[4]} students in this range, which is  {(double)Counts[4] / total:P}");
+            Console.WriteLine($" A (First Class) > There are {Counts[0]} students in this range, which is {(int)Counts[0] / total:P}");
+            Console.WriteLine($" B (Upper Second Class) > {Counts[1]} students in this range, which is {(int)Counts[1] / total:P}");
+            Console.WriteLine($" C (Lower Second Class) > {Counts[2]} students in this range, which is  {(int)Counts[2] / total:P}");
+            Console.WriteLine($" D (Third Class) > {Counts[3]} students in this range, which is  {(int)Counts[3] / total:P}");
+            Console.WriteLine($" F (Fail) > {Counts[4]} students in this range, which is  {(int)Counts[4] / total:P}");
 
             Console.WriteLine();
             Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Press any key to continue");
+            Console.WriteLine("Press a key");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.ReadKey();
-            DisplayMenue();
+            DisplayMenu();
         }
        
 
@@ -128,7 +143,8 @@ namespace ConsoleAppProject.App03
                 int mark = 0;
                 while (!validInput)
                 {
-                    Console.WriteLine("Enter the mark..");
+                    Console.WriteLine("Enter the marks for..  " + Students[i] );
+
                     string input = Console.ReadLine();
                     if (!int.TryParse(input, out mark))
                     {
@@ -136,7 +152,7 @@ namespace ConsoleAppProject.App03
                     }
                     else if (mark > 100 || mark < 0)
                     {
-                        Console.WriteLine("Error");
+                        Console.WriteLine("Error!");
                     }
                     else
                     {
@@ -145,27 +161,36 @@ namespace ConsoleAppProject.App03
                 }
                 Marks[i] = mark;
             }
-            DisplayMenue();
+            DisplayMenu();
 
         }
 
         public void OutputMarks()
         {
+            Console.WriteLine("Names".PadRight(14) + "Marks".PadRight(8) + "Grade");
+            Console.WriteLine("===".PadRight(14)   + "====".PadRight(8)  + "====");
+            int students = 0;
             for (int i = 0; i < Marks.Length; i++)
             {
-                Grades outputGrade = ConvertMarktoGrades(Marks[i]);
-                Console.WriteLine($"{Students[i]} ------- Mark = {Marks[i]} Grade = {outputGrade}");
-                Console.WriteLine($"{Students[i]} ------- Mark = {Marks[i]} Grade = {outputGrade}");
-                Console.WriteLine($"{Students[i]} ------- Mark = {Marks[i]} Grade = {outputGrade}");
-                Console.WriteLine($"{Students[i]} ------- Mark = {Marks[i]} Grade = {outputGrade}");
-
+                int mark = Marks[i];
+                Grades grades = ConvertMarktoGrades(mark);
+                Console.Write($"{Students[i]}".PadRight(13));
+                Console.Write($"{Marks[i]}".PadRight(8));
+                Console.Write($"{grades}".PadRight(1));
                 Console.WriteLine();
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Press any key to continue");
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.ReadKey();
-                DisplayMenue();
+                students++;
+
+
             }
+
+            Console.WriteLine($"These are the {students}" + " students. ");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press a key");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ReadKey();
+            DisplayMenu();
+
 
         }
 
@@ -257,11 +282,45 @@ namespace ConsoleAppProject.App03
 
         public void CalculateGradeProfile()
         {
-            for (int i = 0; i < GradeProfile.Length; i++)
+            
+
+            foreach (int mark in Marks)
             {
-                GradeProfile[i] = 0;
+                if (mark >= FIRSTCLASS)
+                {
+                    Counts[0]++;
+                }
+                else if (mark >= UPPERSECONDCLASS)
+                {
+                    Counts[1]++;
+                }
+                else if (mark >= LOWERSECONDCLASS)
+                {
+                    Counts[2]++;
+                }
+                else if (mark >= THIRDCLASS)
+                {
+                    Counts[3]++;
+                }
+                else
+                {
+                    Counts[4]++;
+                }
             }
 
+            foreach (int mark in Marks)
+            {
+                Grades grade = ConvertToGrade(mark);
+                GradeProfile[(int)grade]++;
+            }
+        }
+
+        public void CalculateGradeProfileUnitTest()
+        {
+            for (int i = 0; i < Counts.Length; i++)
+            {
+                Counts[i] = 0;
+            }
             foreach (int mark in Marks)
             {
                 Grades grade = ConvertToGrade(mark);
